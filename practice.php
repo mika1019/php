892a64bf5784190a44edaa6925a342ba793a3448
mika <smile.happiness8484@gmail.com>
2019-10-18 08:22:03 +0000

    first commit

---
+++ practice.php
@@ -0,0 +1,33 @@
+<?php
+echo 'hello php!';
+$a=3;
+$b=7;
+echo $a+$b;
+
+$array_month=["January","February","March","April","May","June","July","August","September","October","November","December"];
+print $array_month[7];
+
+$hello="hello";
+$name="mika";
+$world="‘s World!";
+echo $hello.$name.$world;
+
+$tech_boost="tech";
+$tech_boost.=" boost";
+echo $tech_boost;
+
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+echo $calendar_2018["December"];
